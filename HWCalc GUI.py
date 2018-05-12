b278504685e039ab055da38e5fa8935523be3526
BriandBlalock <briandblalock@gmail.com>
2018-05-12 17:03:43 -0400

    fixes
    fixed writing toggle on and off. mad grid size easier to resize

--- HWCalc GUI.py
+++ HWCalc GUI.py
@@ -12,8 +12,9 @@ class GUI:
 
     input_row_offset = 5
     input_column_offset = 5
+    grid_size = 20
 
-    is_writing = True
+    is_writing =  False
 
     def create_display(self):
 
@@ -34,41 +35,43 @@ class GUI:
 
             color = "#cccccc"
 
-
+            """
             if( y-1>=0 and self.grid[x][y-1]['bg']=='white'):
                 self.grid[x][y - 1]['bg'] = color
 
-            if (y+1<16 and self.grid[x][y+1]['bg'] == 'white' ):
+            if (y+1<self.grid_size and self.grid[x][y+1]['bg'] == 'white' ):
                 self.grid[x][y + 1]['bg'] = color
 
             if (x-1>=0 and self.grid[x-1][y]['bg'] == 'white' ):
                 self.grid[x-1][y]['bg'] = color
 
-            if (x+1 <16 and self.grid[x+1][y]['bg'] == 'white'):
+            if (x+1 <self.grid_size and self.grid[x+1][y]['bg'] == 'white'):
                 self.grid[x+1][y]['bg'] = color
+            """
+
+            
+
+    def toggle_writing(self, Event):
+        self.is_writing = not(self.is_writing)
 
 
 
-    def toggle_writing_on(self, Event=None):
-        is_writing = True
-
-    def toggle_writing_off(self, Event=None):
-        is_writing = False
 
     def create_grid(self):
 
         # frame.bind  ``("<Button-1>", self.toggle_writing_on)
         # frame.bind("<ButtonRelease-1>", self.toggle_writing_off)
 
-        for x in range(16):
+        for x in range(self.grid_size):
 
             list = []
 
-            for y in range(16):
+            for y in range(self.grid_size):
 
                 list.append( Button(root, bd = 1, text="   ", bg='white') )
 
                 list[y].bind("<Motion>", self.fill)
+                list[y].bind("<ButtonPress-1>", self.toggle_writing)
                 list[y].grid( row=x+self.input_row_offset, column=y+self.input_column_offset, padx=0, pady=0 )
 
             self.grid.append(list)
@@ -77,40 +80,33 @@ class GUI:
 
     def clear_grid(self, Event):
 
-        for x in range(16):
+        for x in range(self.grid_size):
 
-            for y in range(16):
+            for y in range(self.grid_size):
 
                 self.grid[x][y].configure(bg= 'white')
 
     def create_buttons(self):
 
         clear = Button (root, text = "clear")
-        clear.grid( row = 6, column = 24)
+        clear.grid( row = 6, column = self.grid_size+6)
         clear.bind("<Button-1>", self.clear_grid)
 
 
     def __init__(self, root):
 
-        root.geometry('400x450')
+        root.geometry('400x500')
         root.resizable(width=False, height=False)
-        root.bind_all("<ButtonPress-1>",self.toggle_writing_on() )
         root.bind_all("<ButtonRelease-1>", self.toggle_writing_off())
+
         self.create_display()
         self.create_grid()
         self.create_buttons()
 
-
-
-
-
-
-
-
 def main ():
 
-
     gui = GUI(root)
+
     root.mainloop()
 
 
